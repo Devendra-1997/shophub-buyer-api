1f41a1b95c67150afd0c2b1ce2bb0cc0a4ff454e
Devendra-1997 <devendradhakal92@gmail.com>
2024-11-09 07:54:26 +1100

    added error handler

---
+++ router/routers.js
@@ -0,0 +1,7 @@
+import userRouter from "./userRouter.js";
+export const routes = [
+  {
+    path: "/users",
+    middlewares: [userRouter],
+  },
+];
--- router/userRouter.js
+++ router/userRouter.js
@@ -3,6 +3,13 @@ import express from "express";
 const router = express.Router();
 
 // signup
+router.post("/signup", (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+
 // login
 // verify-account
 // otp request
