7eeee185bc22ecf6d6bb9257f41f295751a3b531
Devendra-1997 <devendradhakal92@gmail.com>
2024-11-08 21:46:49 +1100

    database connected

---
+++ config/dbConfig.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+export const mongoConnect = () => {
+  try {
+    mongoose.connect(process.env.MONGO_URI) && console.log("DB Connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { mongoConnect } from "./config/dbConfig.js";
 
 const app = express();
 const PORT = process.env.PORT || 9000;
@@ -8,6 +9,9 @@ const PORT = process.env.PORT || 9000;
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+// connect to mongoDB
+mongoConnect();
 
 // Run the server
 app.listen(PORT, (error) => {
