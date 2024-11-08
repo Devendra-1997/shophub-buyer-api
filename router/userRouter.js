f2b162523d87e2389c6e7d691da8a561e3275b06
Devendra-1997 <devendradhakal92@gmail.com>
2024-11-08 21:58:48 +1100

    added nodemailer/bcrypt

---
+++ router/userRouter.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const router = express.Router();
+
+// signup
+// login
+// verify-account
+// otp request
+// reset-password
+// update-profile
+// delete-account
+export default router;
